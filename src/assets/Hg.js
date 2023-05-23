7c10a44e120cf3c9197af657388329e9f584441a
thmansaleh <thman.saleh@gmail.com>
2023-05-24 01:22:55 +0400

    Initial commit

--- src/assets/Hg.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Othman = () => {
-  return (
-    <div>Hg</div>
-  )
-}
-
-export default Othman
--- src/seactions/first-section/images-section/Images.js
+++ src/seactions/first-section/images-section/Images.js
@@ -1,4 +1,7 @@
 import image1 from '../../../assets/image1.jpeg'
+import image2 from '../../../assets/image2.jpg'
+import image3 from '../../../assets/image3.jpg'
+import image4 from '../../../assets/image4.jpeg'
 import './style.css'
 
 
@@ -8,11 +11,11 @@ const Images = () => {
         <div className="images-container">
             <div className='left'>
                 <img src={image1} alt='img' />
-                <img  src={image1} alt='img' />
+                <img  src={image4} alt='img' />
             </div>
             <div className='right'>
-                <img  src={image1} alt='img' />
-                <img  src={image1} alt='img' />
+                <img  src={image2} alt='img' />
+                <img  src={image3} alt='img' />
             </div>
 
 
